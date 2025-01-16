9719d24f3d2481ccf310bf2c05c63e2effe0d21f
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2025-01-16 02:16:15 +0100

    export path

--- cmake/imgui.cmake
+++ cmake/imgui.cmake
@@ -10,4 +10,14 @@ FetchContent_MakeAvailable(ImGui)
 
 if (NOT TARGET ImGui)
   add_library(ImGui   INTERFACE )
+
+  include_directories( ${imgui_SOURCE_DIR} ${imgui_SOURCE_DIR}/backends )
+
+  if(hasParent)
+    message( INFO " EXPORT ImGui SRC DIR: [${imgui_SOURCE_DIR}]" )
+    message( INFO " EXPORT ImGui BIN DIR: [${imgui_BINARY_DIR}]" )
+    # make both ${imgui_SOURCE_DIR} and ${imgui_BINARY_DIR} visible at parent scope.
+    set( imgui_SOURCE_DIR ${imgui_SOURCE_DIR} PARENT_SCOPE )
+    set( imgui_BINARY_DIR ${imgui_BINARY_DIR} PARENT_SCOPE )
+  endif(hasParent)  
 endif()
